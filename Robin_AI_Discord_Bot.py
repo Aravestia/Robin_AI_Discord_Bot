05fc6226d4bb60659618e9dba443ab806c46e27c
Aloysius Goh <aloysiusgohkw@gmail.com>
2024-09-03 00:45:22 +0800

    try excepts

--- Robin_AI_Discord_Bot.py
+++ Robin_AI_Discord_Bot.py
@@ -252,66 +252,89 @@ async def queue(ctx, *search_query):
 
 @bot.command(name='showqueue', help='check queue status')
 async def showqueue(ctx):
-    global song_queue
-    guild = ctx.message.guild.id
-    msg = "Songs in queue: \n"
-    
-    if len(song_queue[guild]) == 0:
-        msg = "No songs in queue."
-    
-    for i in range(len(song_queue[guild])):
-        msg = msg + f"**{i + 1}** - {song_queue[guild][i]} \n"
+    try:
+        global song_queue
+        guild = ctx.message.guild.id
+        msg = "Songs in queue: \n"
         
-    await ctx.send(msg)
+        if guild not in song_queue:
+            msg = "No songs in queue."
+        else:
+            if len(song_queue[guild]) == 0:
+                msg = "No songs in queue."
+            else:
+                for i in range(len(song_queue[guild])):
+                    msg = msg + f"**{i + 1}** - {song_queue[guild][i]} \n"
+            
+        await ctx.send(msg)
+    except Exception as e:
+        await ctx.send(f"Sorry, there is an error with my program: **{e}**")
+        print(f"showqueue Error: {e}")
 
 @bot.command(name='stop', help='stops song and clears song queue')
 async def stop(ctx):
-    global song_queue
-    guild = ctx.message.guild.id
+    try:
+        global song_queue
+        guild = ctx.message.guild.id
 
-    voice_client = ctx.message.guild.voice_client
-    if voice_client.is_paused():
-        voice_client.resume()
-    
-    if voice_client.is_playing():
-        await ctx.send("Sure, I'll stop singing.")
-        song_queue[guild].clear()
-        voice_client.stop()
-    else:
-        await ctx.send("I'm not singing anything at the moment...")
+        voice_client = ctx.message.guild.voice_client
+        if voice_client.is_paused():
+            voice_client.resume()
+        
+        if voice_client.is_playing():
+            await ctx.send("Sure, I'll stop singing.")
+            song_queue[guild].clear()
+            voice_client.stop()
+        else:
+            await ctx.send("I'm not singing anything at the moment...")
+    except Exception as e:
+        await ctx.send(f"Sorry, there is an error with my program: **{e}**")
+        print(f"stop Error: {e}")
         
 @bot.command(name='skip', help='skips the current song')
 async def skip(ctx):
-    global song_queue
-    guild = ctx.message.guild.id
-    voice_client = ctx.message.guild.voice_client
-    if voice_client.is_playing():
-        
-        if len(song_queue[guild]) < 1:
-            await ctx.send("Sure, I'll stop singing.")
+    try:
+        global song_queue
+        guild = ctx.message.guild.id
+        voice_client = ctx.message.guild.voice_client
+        if voice_client.is_playing():
+            
+            if len(song_queue[guild]) < 1:
+                await ctx.send("Sure, I'll stop singing.")
+            else:
+                await ctx.send("All right, I'll switch to the next song.")
+            voice_client.stop()
         else:
-            await ctx.send("All right, I'll switch to the next song.")
-        voice_client.stop()
-    else:
-        await ctx.send("I'm not singing anything at the moment...") 
+            await ctx.send("I'm not singing anything at the moment...")
+    except Exception as e:
+        await ctx.send(f"Sorry, there is an error with my program: **{e}**")
+        print(f"skip Error: {e}")
 
 @bot.command(name='pause', help='pauses song')
 async def pause(ctx):
-    voice_client = ctx.message.guild.voice_client
-    if voice_client.is_playing():
-        voice_client.pause()
-        await ctx.send("Got it, I'll pause singing.")
-    else:
-        await ctx.send("I'm not singing anything at the moment...")
+    try:
+        voice_client = ctx.message.guild.voice_client
+        if voice_client.is_playing():
+            voice_client.pause()
+            await ctx.send("Got it, I'll pause singing.")
+        else:
+            await ctx.send("I'm not singing anything at the moment...")
+    except Exception as e:
+        await ctx.send(f"Sorry, there is an error with my program: **{e}**")
+        print(f"pause Error: {e}")
 
 @bot.command(name='resume', help='resumes song')
 async def resume(ctx):
-    voice_client = ctx.message.guild.voice_client
-    if voice_client.is_paused():
-        voice_client.resume()
-        await ctx.send("Got it, I'll resume singing.")
-    else:
-        await ctx.send("I'm not singing anything at the moment...")
+    try:
+        voice_client = ctx.message.guild.voice_client
+        if voice_client.is_paused():
+            voice_client.resume()
+            await ctx.send("Got it, I'll resume singing.")
+        else:
+            await ctx.send("I'm not singing anything at the moment...")
+    except Exception as e:
+        await ctx.send(f"Sorry, there is an error with my program: **{e}**")
+        print(f"resume Error: {e}")
 
 # Fun commands
 
@@ -340,39 +363,55 @@ magic_8ball_list = [
 
 @bot.command(name='hi', help='says hi to your friend!')
 async def hi(ctx, *, fname: str = None):
-    if fname == None:
-        await ctx.send(f"hi, {ctx.message.author.name}!")
-    else:
-        await ctx.send(f"hi, {fname}!")
+    try:
+        if fname == None:
+            await ctx.send(f"hi, {ctx.message.author.name}!")
+        else:
+            await ctx.send(f"hi, {fname}!")
+    except Exception as e:
+        await ctx.send(f"Sorry, there is an error with my program: **{e}**")
+        print(f"hi Error: {e}")
     
 @bot.command(name='roll', help='rolls a die')
 async def roll(ctx):
-    r = random.randint(1, 6)
-    await ctx.send(f"**Aventurine:** *How about a game? Nothing fancy, just a game of dice🎲 to gauge today's luck.*")
-    time.sleep(1)
-    await ctx.send(f"{ctx.message.author.name}'s roll: **{r}**")
-    
+    try:
+        r = random.randint(1, 6)
+        await ctx.send(f"**Aventurine:** *How about a game? Nothing fancy, just a game of dice🎲 to gauge today's luck.*")
+        time.sleep(1)
+        await ctx.send(f"{ctx.message.author.name}'s roll: **{r}**")
+    except Exception as e:
+        await ctx.send(f"Sorry, there is an error with my program: **{e}**")
+        print(f"roll Error: {e}")
+        
 @bot.command(name='8ball', help='asks magic 8 ball a question')
 async def magic_8ball(ctx, *, qn: str = None):
-    if qn is not None:
-        r = random.randint(0, 19)
-        await ctx.send(f"*{magic_8ball_list[r]}*")
-    else:
-        await ctx.send(f"*The magic 8 ball is waiting eagerly...*")
+    try:
+        if qn is not None:
+            r = random.randint(0, 19)
+            await ctx.send(f"*{magic_8ball_list[r]}*")
+        else:
+            await ctx.send(f"*The magic 8 ball is waiting eagerly...*")
+    except Exception as e:
+        await ctx.send(f"Sorry, there is an error with my program: **{e}**")
+        print(f"8ball Error: {e}")
         
 @bot.command(name='wiki', help='consults wikipedia')
 async def wiki(ctx, *search_query):
-    page = wikipedia.page("_".join(search_query))
-    
-    await ctx.send("**Dan Heng:** *I'll take a look in the archives📖.*")
     try:
-        if page.exists():
-            await ctx.send(f"{page.summary[0:1000]}... \n\n**Dan Heng:** *Let me know if you need any more help.*")
-        else:
-            await ctx.send("**Dan Heng:** *Sorry, I could not find any info on that topic...*")
+        page = wikipedia.page("_".join(search_query))
+        
+        await ctx.send("**Dan Heng:** *I'll take a look in the archives📖.*")
+        try:
+            if page.exists():
+                await ctx.send(f"{page.summary[0:1000]}... \n\n**Dan Heng:** *Let me know if you need any more help.*")
+            else:
+                await ctx.send("**Dan Heng:** *Sorry, I could not find any info on that topic...*")
+        except Exception as e:
+            await ctx.send(f"Sorry, there is an error with my program: **{e}**")
+            print(f"Error: {e}")
     except Exception as e:
         await ctx.send(f"Sorry, there is an error with my program: **{e}**")
-        print(f"Error: {e}")  
+        print(f"wiki Error: {e}")
   
 # Debug commands
 
@@ -385,7 +424,11 @@ async def debug_show_all_queue(ctx):
     
 @bot.command(name='~debug_get_guild_id')
 async def debug_get_guild_id(ctx):
-    await ctx.send(f"Your server's id: {ctx.message.guild.id}")
+    try:
+        await ctx.send(f"Your server's id: {ctx.message.guild.id}")
+    except Exception as e:
+        await ctx.send(f"Sorry, there is an error with my program: **{e}**")
+        print(f"~debug_get_guild_id Error: {e}")
     
 TOKEN = os.getenv('ROBIN_AI_DISCORD_TOKEN')
 if TOKEN: 
